1e4e3f6ef6b135474e6dd0581828c3f818935314
Patrick Ford <patrickford@msn.com>
2016-07-29 22:08:26 -0700

    added jedi name

--- index.js
+++ index.js
@@ -6,4 +6,16 @@ app.get('/', function(request, response) {
     response.send("Hello World");
 });
 
+app.get('/:firstname/:lastname', function(request, response) {
+    var first = request.params.firstname;
+    var last = request.params.lastname;
+    response.send(["Hello", first, last].join(" "));
+});
+
+app.get('/jedi/:firstname/:lastname', function(request, response) {
+    var first = request.params.firstname.slice(0,2);
+    var last = request.params.lastname.slice(0,3);
+    response.send("Hello " + last + first);
+});
+
 app.listen(8080);
